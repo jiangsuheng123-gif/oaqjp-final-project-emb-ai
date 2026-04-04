877a3347cfd13bfbe073aabf5bfcfa343b1a3980
Perry <jiangsuheng123@gmail.com>
2026-04-04 01:16:35 -0400

    package the app

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotion_predictions['anger']
+    disgust_score = emotion_predictions['disgust']
+    fear_score = emotion_predictions['fear']
+    joy_score = emotion_predictions['joy']
+    sadness_score = emotion_predictions['sadness']
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_scores.items(), key=lambda x: x[1])[0]
+    emotion_scores['dominant_emotion'] = dominant_emotion
+    return emotion_scores
