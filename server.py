1759e42b65cedfbd299261c93431ac5b8a61babc
Perry <jiangsuheng123@gmail.com>
2026-04-05 14:49:19 +0800

    pylint check

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Server module for Emotion Detector application."""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +6,9 @@ app = Flask("EmotionDetectorServer")
 
 @app.route("/emotionDetector")
 def emotion_detector_route():
+    """Route for emotion detection."""
     text_to_analyze = request.args.get('textToAnalyze')
-    
     response = emotion_detector(text_to_analyze)
-    
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
@@ -23,12 +23,13 @@ def emotion_detector_route():
         f"'joy': {joy} and 'sadness': {sadness}. "
         f"The dominant emotion is {dominant_emotion}."
     )
-    
     return response
 
 @app.route("/")
 def index():
+    """Route for index page."""
     return render_template("index.html")
 
+# Main function
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
